94e71f9b7cbac7ce45e667d3fe0e0163aad34ec0
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-09 11:23:19 +0530

    Export static HTML file

---
+++ src/app/userDetails/products.js
@@ -0,0 +1,10 @@
+import User from "@/components/User";
+
+export default function products() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <User />
+    </div>
+  );
+}
---
+++ src/components/User.js
@@ -0,0 +1,7 @@
+export default function User() {
+  return (
+    <div>
+      <h1>Hello user components</h1>
+    </div>
+  );
+}
